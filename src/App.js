e8c320bf7ab82525c01c5ced8d1999f9528ed586
Codebadger99 <ebukanuel2021@gmail.com>
2023-01-08 15:55:09 -0800

    Random-user_Generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [Firstname, setFirstname] = useState("");
+  const [Lastname, setLastname] = useState("");
+  const [MaidenName, setMaidenName] = useState("");
+  const [Age, setAge] = useState("");
+  const [Gender, setGender] = useState("");
+  const [Phone, setPhone] = useState("");
+  const [Image, setImage] = useState("");
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = () => {
+    fetch("https://dummyjson.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        let dataUsers = data.users;
+        let randnum = Math.floor(Math.random() * dataUsers.length);
+        let randusers = dataUsers[randnum];
+        setFirstname(randusers.firstName);
+        setLastname(randusers.lastName);
+        setMaidenName(randusers.maidenName);
+        setAge(randusers.age);
+        setGender(randusers.gender);
+        setPhone(randusers.phone);
+        setImage(randusers.image);
+      });
+  };
+
+  const handleClick = () => {
+    getUsers();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img
+            src={Image}
+            className="user-img"
+            alt="user-img"
+            style={{ backgroundImage: `url(${Image})` }}
+          />
+          <p className="user-title">
+            {Firstname} {Lastname} {MaidenName}
+          </p>
+          <p className="user-age">Age: {Age}</p>
+          <p className="user-gender">Gender: {Gender}</p>
+          <p className="user-phone">Phone: {Phone}</p>
+          <button className="btn" type="submit" onClick={handleClick}>
+            New User
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
